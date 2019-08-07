83470770515f8f242d726029eca02decda483d62
Phil Davies <github@phildavies.co.uk>
2019-08-07 11:06:56 +0100

    double auth poc wip

--- DoubleTroubleClient/Program.cs
+++ DoubleTroubleClient/Program.cs
@@ -1,9 +1,41 @@
-﻿namespace DoubleTroubleClient
+﻿using System;
+using System.Net.Http;
+using SFA.DAS.Http;
+using SFA.DAS.Http.Configuration;
+using SFA.DAS.Http.TokenGenerators;
+
+namespace DoubleTroubleClient
 {
     internal class Program
     {
         public static void Main(string[] args)
         {
+            var jwtHttpClient = GetHttpClient();
+        }
+        
+        private static HttpClient GetHttpClient()
+        {
+            var config = new JwtClientConfiguration
+            {
+                BaseUrl = "https://localhost:44321/",
+                ClientToken = "don't check in!"
+            };
+
+            var httpClient = new HttpClientBuilder()
+                .WithBearerAuthorisationHeader(new JwtBearerTokenGenerator(config))
+                .WithDefaultHeaders()
+                .Build();
+
+            httpClient.BaseAddress = new Uri(config.BaseUrl);
+            
+            return httpClient;
         }
     }
+    
+    public class JwtClientConfiguration : IJwtClientConfiguration
+    {
+        public string BaseUrl { get; set; }
+        public string ClientToken { get; set; }
+    }
+
 }
